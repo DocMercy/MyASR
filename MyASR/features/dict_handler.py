281479b8b6ef89f939f110eb19c3a6737ecc7270
DocMercy <58731303+DocMercy@users.noreply.github.com>
2020-01-28 18:05:43 +0800

    Add files via upload

--- MyASR/features/dict_handler.py
+++ MyASR/features/dict_handler.py
@@ -10,19 +10,19 @@ class DictHandler:
         self.root_path = root_path
         self.dict = {}
         self.count = 1
-
+    
     def add_to_dict(self, data):
         """
         传入需要是str或者list
         """
         for i in data:
             if self.dict.get(i) is None:
-                self.dict[i] = self.count
+                self.dict[i] = str(self.count)
                 self.count += 1
-
-    def build_dict(self, type):
-        label_path = os.path.join(self.root_path, 'label', type)
-
+    
+    def build_dict(self):
+        label_path = os.path.join(self.root_path, 'label')
+        
         for root, dirs, files in os.walk(label_path):
             for file in files:
                 with open(os.path.join(root, file), 'rb') as a:
@@ -31,13 +31,13 @@ class DictHandler:
         print('字典构建完成')
         dict_path = os.path.join(self.root_path, 'dict')
         path_check(dict_path)
-        with open(os.path.join(dict_path, type+'_dict.pkl'), 'wb') as a:
+        with open(os.path.join(dict_path, 'dict.pkl'), 'wb') as a:
             pickle.dump(self.dict, a)
         print('字典保存完成')
 
 
 if __name__ == '__main__':
     d = DictHandler('../temp/test')
-    d.build_dict('chara')
+    d.build_dict()
     d = DictHandler('../temp/train')
-    d.build_dict('chara')
+    d.build_dict()
--- MyASR/features/thchs30_reader.py
+++ MyASR/features/thchs30_reader.py
@@ -11,7 +11,7 @@ class Thchs30Reader:
         self.x_train_path, self.y_train_path = self.get_paths(os.path.join(root_path, 'train'))
         self.x_test_path, self.y_test_path = self.get_paths(os.path.join(root_path, 'test'))
         self.sr = 0
-
+    
     def get_paths(self, path):
         x_result = []
         y_result = []
@@ -23,7 +23,7 @@ class Thchs30Reader:
                     else:
                         x_result.append(os.path.join(root, file))
         return x_result, y_result
-
+    
     def read_audio(self, paths, target_dir):
         target_dir = os.path.join(target_dir, 'audio_raw')
         path_check(target_dir)
@@ -43,50 +43,46 @@ class Thchs30Reader:
                 result = []
                 count += 1
                 print(f'{i + 1}/{len(paths)}条音频处理完成')
-
-    def read_label(self, paths, target_dir):
-        chara_target_path = os.path.join(target_dir, 'label', 'chara')
-        phone_target_path = os.path.join(target_dir, 'label', 'phone')
+    
+    def read_label(self, paths, target_dir, target_type):
+        chara_target_path = os.path.join(target_dir, 'label')
         path_check(chara_target_path)
-        path_check(phone_target_path)
-
-        chara_result = []
-        phone_result = []
+        
+        result = []
         count = 0
         print(f'开始读取{len(paths)}条标签')
-
+        
         for i in range(len(paths)):
             with open(paths[i], 'r', encoding='utf-8') as a:
                 data = a.readlines()
-                chara_result.append(data[0].replace('\n', '').replace(' ', ''))
-                phone_result.append(data[1].replace('\n', '').split(' '))
-
+                if target_type == 'chara':
+                    result.append(data[0].replace('\n', '').replace(' ', ''))
+                else:
+                    result.append(data[1].replace('\n', '').replace(' ', ''))
+            
             if ((i + 1) % 1000 == 0 and i != 0) or (i + 1) == len(paths):
                 with open(os.path.join(chara_target_path, f'label_{count}.pkl'), 'wb') as a:
-                    pickle.dump(chara_result, a)
-                with open(os.path.join(phone_target_path, f'label_{count}.pkl'), 'wb') as a:
-                    pickle.dump(phone_result, a)
-                chara_result = []
-                phone_result = []
+                    pickle.dump(result, a)
+                result = []
                 count += 1
                 print(f'{i + 1}/{len(paths)}条标签处理完成')
-
-    def start_reading(self):
+    
+    def start_reading(self, type):
         train_save_path = os.path.join(self.target_path, 'train')
         test_save_path = os.path.join(self.target_path, 'test')
         path_check(train_save_path)
         path_check(test_save_path)
-
+        
         self.read_audio(self.x_train_path, train_save_path)
         self.read_audio(self.x_test_path, test_save_path)
-
-        self.read_label(self.y_train_path, train_save_path)
-        self.read_label(self.y_test_path, test_save_path)
-
+        
+        self.read_label(self.y_train_path, train_save_path, type)
+        self.read_label(self.y_test_path, test_save_path, type)
+        
         return self.sr
 
 
 if __name__ == '__main__':
-    t = Thchs30Reader(r'D:\Data\data_thchs30', '../temp')
-    sr = t.start_reading()
+    t = Thchs30Reader(r'E:\Data\data_thchs30', '../temp')
+    sr = t.start_reading('chara')
     print(f'音频采样率：{sr}')
