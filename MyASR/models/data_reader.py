a6ec944677f26edd996ea0c8e4599d32556612f3
DocMercy <58731303+DocMercy@users.noreply.github.com>
2020-02-01 13:53:07 +0800

    Add files via upload

--- MyASR/models/data_reader.py
+++ MyASR/models/data_reader.py
@@ -4,63 +4,55 @@ import numpy as np
 
 
 class DataReader:
-    def __init__(self, root_path):
+    def __init__(self, root_path, dict_path, label_type):
         self.root_path = root_path
-        self.x_paths, self.y_paths = self.get_x_y_paths()
-        self.dict, self.reverse_dict = self.read_dict()
-    
-    def read_dict(self):
-        dict_path = os.path.join(self.root_path, 'dict', 'dict.pkl')
+        self.label_type = label_type
+        self.x_paths, self.y_paths, self.y_lens = self.get_x_y_paths()
+        self.dict, self.reverse_dict = self.read_dict(dict_path)
+
+    @staticmethod
+    def read_dict(dict_path):
         with open(dict_path, 'rb') as a:
             dict = pickle.load(a)
-        dict[' '] = 0
         reverse_dict = {}
         for i, j in dict.items():
             reverse_dict[j] = i
         return dict, reverse_dict
-    
+
     def get_x_y_paths(self):
         x_result = []
         y_result = []
+        y_len = []
         x_root = os.path.join(self.root_path, 'x_handled')
-        y_root = os.path.join(self.root_path, 'label')
+        y_root = os.path.join(self.root_path, 'y_handled', self.label_type)
         for root, dirs, files in os.walk(x_root):
             for file in files:
                 x_result.append(os.path.join(root, file))
         for root, dirs, files in os.walk(y_root):
             for file in files:
-                y_result.append(os.path.join(root, file))
-        return x_result, y_result
-    
-    def get_label_length(self, labels, label_pad_len):
-        label_lengths = []
-        y = []
-        
-        for item in labels:
-            label_lengths.append(len(item))
-            result = []
-            diff = label_pad_len - len(item)
-            for i in item:
-                result.append(self.dict[i])
-            result.extend(['0'] * diff)
-            y.append(result)
-        return y, label_lengths
-    
-    def sample_x_y(self, batch_size, label_pad_len):
+                if 'len' in file:
+                    y_len.append(os.path.join(root, file))
+                else:
+                    y_result.append(os.path.join(root, file))
+        return x_result, y_result, y_len
+
+    def sample_x_y(self, batch_size):
         rand_file_num = np.random.randint(len(self.x_paths))
         with open(self.x_paths[rand_file_num], 'rb') as a:
             x_data = pickle.load(a)
         with open(self.y_paths[rand_file_num], 'rb') as b:
             y_data = pickle.load(b)
-        
+        with open(self.y_lens[rand_file_num], 'rb') as b:
+            y_len = pickle.load(b)
+
         rand_batch_num = np.random.randint(0, len(x_data) - batch_size)
         x_this_batch = x_data[rand_batch_num:rand_batch_num + batch_size]
         y_this_batch = y_data[rand_batch_num:rand_batch_num + batch_size]
-        y_this_batch, label_lengths = self.get_label_length(y_this_batch, label_pad_len)
+        label_lengths = y_len[rand_batch_num:rand_batch_num + batch_size]
         return x_this_batch, y_this_batch, label_lengths
 
 
 if __name__ == '__main__':
-    d = DataReader('../temp/test')
+    d = DataReader('../temp/train', '../temp/train/dict/dict.pkl', 'chara')
     print(d.dict)
     print(d.sample_x_y(64)[:5])
--- MyASR/models/model1.py
+++ MyASR/models/model1.py
@@ -7,43 +7,41 @@ class Model1:
         self.train_handler = train_handler
         self.batch_size = 16
         self.max_features = 510
-        self.max_labels = 50
         self.n_mfcc = 40
         self.n_layers = 1
         self.rnn_features = 256
         self.dict_len = len(train_handler.dict) + 1
-        self.learning_rate = 1e-4
-    
+        self.learning_rate = 1e-5
+
     def train_start(self):
         x = tf.placeholder(tf.float32, [self.batch_size, self.max_features, self.n_mfcc])
-        y = tf.placeholder(tf.int32, [self.batch_size, self.max_labels])
+        y = tf.placeholder(tf.int32, [self.batch_size, None])
         label_length = tf.placeholder(tf.int32, [self.batch_size])
-        
+
         cell = tf.nn.rnn_cell.GRUCell
         rnn_layer = tf.nn.rnn_cell.MultiRNNCell([cell(self.rnn_features) for _ in range(self.n_layers)])
         output, states = tf.nn.dynamic_rnn(rnn_layer, x, dtype=tf.float32)
-        
+
         logits = tf.layers.dense(output, self.dict_len)
-        
+
         logits_len = [logits.get_shape()[1].value] * self.batch_size
         loss = tf.reduce_mean(tf.nn.ctc_loss_v2(y, logits, label_length, logits_len, logits_time_major=False))
-        
+
         op = tf.train.AdamOptimizer(self.learning_rate)
         train_step = op.minimize(loss)
-        
+
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
             count = 0
             while True:
-                x_this_batch, y_this_batch, label_length_this_batch = self.train_handler.sample_x_y(self.batch_size,
-                                                                                                    self.max_labels)
+                x_this_batch, y_this_batch, label_length_this_batch = self.train_handler.sample_x_y(self.batch_size)
                 train_step.run(feed_dict={
                     x: x_this_batch,
                     y: y_this_batch,
                     label_length: label_length_this_batch
                 })
                 count += 1
-                if count % 100 == 0 and count != 0:
+                if count % 50 == 0 and count != 0:
                     _loss = sess.run(loss, feed_dict={
                         x: x_this_batch,
                         y: y_this_batch,
@@ -52,5 +50,5 @@ class Model1:
                     print(f'{count}个batch完成，loss={_loss}')
 
 
-m = Model1(DataReader('../temp/test'))
+m = Model1(DataReader('../temp/train', '../temp/train/dict/dict.pkl', 'chara'))
 m.train_start()
